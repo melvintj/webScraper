8b371ad031b5c393c367688b38972c3a34635eba
Melvin Jansen <melvinjansen94@gmail.com>
2024-05-21 17:26:04 +0200

    regex + BeautifulSoup4
    Regex and beautifulsoup4 usage

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Guide; https://realpython.com/python-web-scraping-practical-introduction/#check-your-understanding
 from urllib.request import urlopen
 import re
 
@@ -5,7 +6,7 @@ import re
 def get_html_page_decoded(url):
     page = urlopen(url)
     html = page.read().decode()  # reads the page and decodes the page (because it is in UTF format which is unreadable.
-    print(html)
+    # print(html)
     return html
 
 
@@ -19,6 +20,7 @@ if title_index != -1:
         end_index = html_scraped.find("</title>")
     else:
         end_index = html_scraped.find("</title >")
+
 else:
     title_index_replace = html_scraped.find("<title >")
     start_index = title_index_replace + len("<title >")
@@ -28,10 +30,26 @@ else:
         end_index = html_scraped.find("</title >")
 
 
+
 title = html_scraped[start_index:end_index]  # title content grabbed with slicing (This is a bad way of doing it.)
-title_tag_including_content = re.findall("<title.*>", html_scraped, re.IGNORECASE)
+title_tag_including_content = re.search("<title.*?>.*?</title.*?>", html_scraped, re.IGNORECASE)
+title_grouping = title_tag_including_content.group()
+title = re.sub("<.*?>", "", title)
 
-print(title_tag_including_content)
-title_content = re.findall("", title_tag_including_content[0], re.IGNORECASE)
+# print(title)  # uncomment this line to play with the regex
 
 
+######################## ABOVE IS REGEX with find() findall(), search(), group(), sub(). ##############################
+############################### BELOW IS THE BEAUTIFULSOUP LIBRARY ####################################################
+
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+
+url = "http://olympus.realpython.org/profiles/dionysus"
+page = urlopen(url)
+html = page.read().decode("utf-8")
+soup = BeautifulSoup(html, "html.parser")
+print(soup.prettify())
+
+print(soup.center)
