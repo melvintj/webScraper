6e9e3146357aac7cc08d0edbf1557cb1f1d21702
Melvin Jansen <melvinjansen94@gmail.com>
2024-05-21 16:20:15 +0200

    commit

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ def get_html_page_decoded(url):
 
 
 html_scraped = get_html_page_decoded(url="http://olympus.realpython.org/profiles/poseidon")
-title_index = html_scraped.find("<title>")
 
+
+title_index = html_scraped.find("<title>")
 if title_index != -1:
     start_index = title_index + len("<title>")
     if html_scraped.find("</title>") != -1:
@@ -27,14 +28,9 @@ else:
         end_index = html_scraped.find("</title >")
 
 
-# print(start_index)
-# print(end_index)
-
-
 title = html_scraped[start_index:end_index]  # title content grabbed with slicing (This is a bad way of doing it.)
-
-
 title_tag_including_content = re.findall("<title.*>", html_scraped, re.IGNORECASE)
+
 print(title_tag_including_content)
 title_content = re.findall("", title_tag_including_content[0], re.IGNORECASE)
 
